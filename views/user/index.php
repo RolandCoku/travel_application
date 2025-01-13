4afc043881109cfd874ea4260ed7793089302891
RolandCoku <roland.coku@fti.edu.al>
2025-01-13 15:13:41 +0100

    Created deleted test content from the user index to pull the changes from the finished page

--- views/user/index.php
+++
@@ -1,9 +0,0 @@
-
-<?php require_once app_path('/includes/layout-header.php')?>
-
-<!-- This is only for testing purposes -->
-<body>
-    <h1>Welcome to the homepage</h1>
-
-
-<?php require_once app_path('/includes/layout-footer.php')?>
