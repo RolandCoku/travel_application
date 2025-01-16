25394f5b751549123fef2810def7858295c5d851
RolandCoku <roland.coku@fti.edu.al>
2025-01-16 14:53:11 +0100

    Refactored the admin dashboard to get the common elements as includes

--- includes/layout-header.php
+++ includes/layout-header.php
@@ -5,10 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= base_url("/css/style.css"); ?>">
+    <link rel="stylesheet" href="<?= base_url("/css/style.css?v=" . time()); ?>">
 
+    <!-- TODO: REMOVE THIS LATER TO LEAVE ONLY THE ARRAY -->
     <?php if (isset($cssFile)): ?>
         <link rel="stylesheet" href="<?= base_url("/css/$cssFile"); ?>">
+    <?php endif; ?>
+
+    <?php if (isset($cssFiles)): ?>
+        <?php foreach ($cssFiles as $cssFile): ?>
+            <link rel="stylesheet" href="<?= base_url("/css/$cssFile"); ?>">
+        <?php endforeach; ?>
     <?php endif; ?>
 
     <link href="<?= base_url("/css/boxicons-2.1.4/css/boxicons.min.css"); ?>" rel="stylesheet">
---
+++ includes/travel-agency-admin-sidebar.php
@@ -0,0 +1,33 @@
+<aside>
+    <div class="top">
+        <div class="logo">
+            <h2>TRAVEL<span class="danger">ADMIN</span></h2>
+        </div>
+        <div class="close" id="close-btn">
+            <span class="material-icons-sharp"> close </span>
+        </div>
+    </div>
+
+    <div class="sidebar">
+        <a href="/travel-agency/admin/dashboard">
+            <span class="material-icons-sharp"> dashboard </span>
+            <h3>Dashboard</h3>
+        </a>
+        <a href="/travel-agency/admin/bookings">
+            <span class="material-icons-sharp"> flight </span>
+            <h3>Bookings</h3>
+        </a>
+        <a href="/travel-agency/admin/reviews">
+            <span class="material-icons-sharp"> reviews </span>
+            <h3>Reviews</h3>
+        </a>
+        <a href="/travel-agency/admin/travel-packages">
+            <span class="material-icons-sharp"> travel_explore </span>
+            <h3>Travel packages</h3>
+        </a>
+        <a href="/logout">
+            <span class="material-icons-sharp"> logout </span>
+            <h3>Logout</h3>
+        </a>
+    </div>
+</aside>
