818d79697dab3e2dc29322e1e2a6be1ec5a0fca9
RolandCoku <roland.coku@fti.edu.al>
2025-01-16 15:09:01 +0100

    Made the AuthMiddleware functions non-static to check and implement remember_me functionality

--- helpers/EmailHelpers.php
+++ helpers/EmailHelpers.php
@@ -14,12 +14,12 @@ class EmailHelpers
         try {
             //Server settings
             $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = $_ENV['SMTP_HOST'];                     // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = $_ENV['SMTP_USERNAME'];                 // SMTP username
-            $mail->Password   = $_ENV['SMTP_PASSWORD'];                 // SMTP password
+            $mail->Host = $_ENV['SMTP_HOST'];                     // Set the SMTP server to send through
+            $mail->SMTPAuth = true;                                   // Enable SMTP authentication
+            $mail->Username = $_ENV['SMTP_USERNAME'];                 // SMTP username
+            $mail->Password = $_ENV['SMTP_PASSWORD'];                 // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port       = $_ENV['SMTP_PORT'];                     // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $mail->Port = $_ENV['SMTP_PORT'];                     // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
             $mail->setFrom($_ENV['SMTP_USERNAME'], 'Travel App');
@@ -28,7 +28,7 @@ class EmailHelpers
             // Content
             $mail->isHTML(true);                                  // Set email format to HTML
             $mail->Subject = $subject;
-            $mail->Body    = $body;
+            $mail->Body = $body;
 
             $mail->send();
             return true;
@@ -40,9 +40,38 @@ class EmailHelpers
     public static function sendConfirmationEmail($email, $token): bool
     {
         $subject = "Confirm your email";
-        $body = "<p>Click the link below to confirm your email</p>
-                <a href='http://localhost/confirm-email?token=$token'>Confirm Email</a>";
+        $body = "
+                <p>Click the button below to confirm your email:</p>
+                <form action='http://localhost/confirm-email' method='POST' style='display: inline;'>
+                    <input type='hidden' name='token' value='$token'>
+                    <button type='submit' style='background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;'>Confirm Email</button>
+                </form>
+                ";
+
 
         return self::sendEmail($email, $subject, $body);
     }
+
+    public static function sendBookingCanceledEmail($email, $bookingName): bool
+    {
+        $subject = "Booking Canceled";
+        $body = "
+        <p>Your booking for $bookingName has been cansceled</p>
+       
+                ";
+
+        return self::sendEmail($email, $subject, $body);
+    }
+
+    public static function sendBookingConfirmedEmail($email, $bookingName): bool
+    {
+        $subject = "Booking Confirmed";
+        $body = "
+                <p>Your booking for $bookingName has been confirmed.</p>
+                ";
+
+        return self::sendEmail($email, $subject, $body);
+    }
+
+
 }
